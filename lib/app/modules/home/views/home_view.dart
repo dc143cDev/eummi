b6752b7dde0cd1b1f55ac749807faa6f4675c95e
dc143cDev <devc143c@gmail.com>
2022-12-30 23:30:43 +0900

    git init

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import 'package:get/get.dart';
 
@@ -9,10 +10,24 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Eummi'),
-        centerTitle: true,
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.add_event,
+        children: [
+          SpeedDialChild(child: Icon(Icons.book), label: '앨범 추가하기'),
+          SpeedDialChild(
+              child: Icon(Icons.sticky_note_2_rounded), label: '노트 추가하기'),
+          SpeedDialChild(child: Icon(Icons.photo), label: '사진 추가하기'),
+        ],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.white,
+      //   onPressed: () {},
+      //   child: Icon(
+      //     Icons.more_horiz,
+      //     color: Color(0xff6667AB),
+      //   ),
+      //   elevation: 10,
+      // ),
       body: Center(
         child: Text(
           'HomeView is working',
